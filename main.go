1d5e9403a4797d7e810b810a0e4d808c12804019
cijin <cijin@seagin.me>
2024-05-13 13:33:40 +0700

    load balancer ready

--- main.go
+++ main.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+const port = "8080"
+
+type loadBalancer struct {
+	backends []*url.URL
+	current  int
+}
+
+func (lb *loadBalancer) nextBackend() *url.URL {
+	// change to round robin
+	return lb.backends[(lb.current+1)%len(lb.backends)]
+}
+
+func (lb *loadBalancer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	backend := lb.nextBackend()
+
+	proxy := httputil.NewSingleHostReverseProxy(backend)
+	proxy.ServeHTTP(w, r)
+}
+
+func main() {
+	backends := []*url.URL{
+		{Scheme: "http", Host: "localhost:8081"},
+		{Scheme: "http", Host: "localhost:8082"},
+	}
+
+	lb := &loadBalancer{
+		backends: backends,
+		current:  0,
+	}
+
+	fmt.Println("load balancer listening on :8080")
+	http.ListenAndServe(":8080", lb)
+}
